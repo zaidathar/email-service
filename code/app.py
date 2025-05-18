6463f86b7aa02d3d47bf4801077179fca145b1eb
zaidathar <codewithzukon@gmail.com>
2025-05-18 18:40:28 +0530

    feat : Added rate limiting for all api , increased health limit to 1000 per minutes for readiness check

--- code/app.py
+++ code/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from code.config import load_config
 from code.utils.logger import logger
 import os
@@ -9,8 +11,14 @@ config = load_config()
 app = Flask(__name__)
 CORS(app, origins=config["allowed_origins"])
 
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    default_limits=["100 per day", "20 per hour", "5 per minute"]
+)
 
 @app.route('/health', methods=['GET'])
+@limiter.limit("1000 per minute")
 def health_check():
     return {"status": "healthy"}, 200
 
